cf12fb85b31276d9aeb844042719001199bea164
John Joseph Ugalino <john.ugalino@obf.ateneo.edu>
2015-03-26 03:27:56 +0800

    major fixes, implementing collision RAFFEH PLS IMPLEMENT NUDGE @ LINE 414

--- src/main v0.2.cpp
+++ src/main v0.2.cpp
@@ -19,29 +19,31 @@ using namespace std;
 
 const int WIDTH = 1200;
 const int HEIGHT = 400;
+const int FPS = 60;
+const float TFPS = 100.0f/FPS;
+const int FLOOR_WIDTH = WIDTH;
+const int FLOOR_HEIGHT = 30;
 const int P1_START_POSX = 100;
 const int P2_START_POSX = WIDTH - 60;
 const int TILE_SIZE = 25;
 const int MAP_ROWS = HEIGHT/TILE_SIZE;
 const int MAP_COLUMNS = WIDTH/TILE_SIZE + 1;
-const int FPS = 60;
-const int PLAYER_W = 25;
-const int PLAYER_H = 50;
-const float TFPS = 100.0f/FPS;
+const float PLAYER_W = 25;
+const float PLAYER_H = 50;
 const float PLAYER_VX = 3.5;
+const float SWORD_INC = PLAYER_H/5;
+const float SWORD_W = 50;
+const float SWORD_H = 5;
+const float SWORD_ACC = 0.5;
 const double GRAV = 9.8/(FPS*1.0);
-//const int FLOOR_WIDTH = WIDTH;
-//const int FLOOR_HEIGHT = 30;
-//const float SWORD_INC = PLAYER_H/5;
-//const float SWORD_W = 50;
-//const float SWORD_H = 5;
-//const float SWORD_ACC = 0.5;
 
 int map_grid[MAP_ROWS][MAP_COLUMNS];
+
 
 sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "Sorta Jousting");
 sf::Clock gameClock;
 sf::Time frameTime;
+sf::RectangleShape shape(sf::Vector2f(FLOOR_WIDTH,FLOOR_HEIGHT));
 
 //some references from http://www.gamefromscratch.com/page/Game-From-Scratch-CPP-Edition.aspx
 //and animation references from https://github.com/LaurentGomila/SFML/wiki/Source:-AnimatedSprite
@@ -83,7 +85,7 @@ private:
     const sf::Texture* m_texture;
     sf::Vertex m_vertices[4];
 public:
-	AnimatedSprite() : m_animation(NULL), m_frameTime(sf::seconds(0.05f)), m_currentFrame(0), m_isPaused(true), m_isLooped(false), m_texture(NULL){}
+	AnimatedSprite() : m_animation(NULL), m_frameTime(sf::seconds(0.001)), m_currentFrame(0), m_isPaused(true), m_isLooped(false), m_texture(NULL){}
 	AnimatedSprite(sf::Time frameTime, bool paused, bool looped) : m_animation(NULL), m_frameTime(frameTime), m_currentFrame(0), m_isPaused(paused), m_isLooped(looped), m_texture(NULL){}
 	~AnimatedSprite(){
 		m_animation = NULL;
@@ -200,43 +202,13 @@ public:
 	}
 };
 
-class TextureLoader{
-protected:
-	map<string, sf::Texture*> textures;
-public:
-	TextureLoader(){};
-	~TextureLoader(){
-		map<string, sf::Texture*>::iterator itr = textures.begin();
-		delete itr->second;
-		textures.erase(itr);
-	};
-
-	sf::Texture* getTexture(string n){
-		sf::Texture* temp = NULL;
-		map<string, sf::Texture*>::const_iterator results = textures.find(n);
-		if(results != textures.end()){
-			temp = results->second;
-		}else{
-			temp = new sf::Texture();
-			temp->loadFromFile(n);
-			temp->setSmooth(true);
-			textures.insert(pair<string, sf::Texture*>(n,temp));
-			return temp;
-		}
-		return temp;
-	}
-};
-
-TextureLoader tl;
-
 class Node{
 protected:
-	sf::Vector2f vel;
+	sf::Vector2f pos, vel, origin;
 	sf::Sprite spr;
+	Animated aspr;
 	sf::Texture txt;
-	AnimatedSprite aspr;
 	string filename;
-	short type;
 	bool loaded;
 	bool cstatic;
 public:
@@ -247,8 +219,15 @@ public:
 		return loaded;
 	}
 	virtual void load(string fn){
-		spr.setTexture(*tl.getTexture(fn));
-		loaded = true;
+		if(txt.loadFromFile(fn) == false){
+			filename = "";
+			loaded = false;
+		}else{
+			filename = fn;
+			txt.setSmooth(true);
+			spr.setTexture(txt);
+			loaded = true;
+		}
 	}
 	virtual void draw(sf::RenderWindow &rw){
 		if(loaded){
@@ -256,18 +235,15 @@ public:
 		}
 	}
 	virtual void update(){
-		return;
-	}
-	virtual void move(){
 		if(loaded){
-			//setPos(pos.x + vel.x*TFPS, pos.y + vel.y*TFPS);
-			//move()
-			spr.move(vel.x*TFPS, vel.y*TFPS);
+			setPos(pos.x + vel.x*TFPS, pos.y + vel.y*TFPS);
 		}
 	}
 	virtual void setPos(float x, float y){
 		if(loaded){
-			spr.setPosition(x,y);
+			pos.x = x;
+			pos.y = y;
+			spr.setPosition(pos);
 		}
 	}
 	virtual void setVel(float x, float y){
@@ -293,18 +269,12 @@ public:
 	}
 	virtual sf::Vector2f getPos(){
 		if(loaded){
-			return spr.getPosition();
+			return pos;
 		}
 		return sf::Vector2f();
 	}
 	virtual sf::Sprite& getSprite(){
 		return spr;
-	}
-	virtual AnimatedSprite& getAnimatedSprite(){
-		return aspr;
-	}
-	virtual void setAnim(int n){
-		return;
 	}
 	virtual bool isColliding(Node* n){
 		sf::Rect<float> mine = getBoundingRect();
@@ -321,11 +291,11 @@ class Player : public Node{
 protected:
 	vector<Animation> anims;
 	vector<sf::Texture> txts;
+	AnimatedSprite spr;
 	Animation* currAnim;
 	bool face;
 public:
-	Player(){
-		aspr = AnimatedSprite(sf::seconds(0.05f),true,false);
+	Player() : spr(sf::seconds(0.05),true,false) {
 		load("textures/idleplayer.png");
 		load("textures/walkplayer.png");
 		for(int i = 0; i < 2; i++){
@@ -335,7 +305,7 @@ public:
 				anims[i].addFrame(sf::IntRect((j%5)*25,(j/5)*50,25,50));
 			}
 		}
-		type = 1;
+
 		currAnim = &anims[0];
 		assert(isLoaded());
 		getAnimatedSprite().setOrigin(12.4998, 25);
@@ -344,35 +314,43 @@ public:
 
 	void update(){
 		if(loaded){
-			aspr.play(*currAnim);
-			aspr.update(frameTime);
+			setPos(pos.x + vel.x*TFPS, pos.y + vel.y*TFPS);
 		}
-	}
-	void move(){
-		if(loaded){
-			//setPos(pos.x + vel.x*TFPS, pos.y + vel.y*TFPS);
-			aspr.move(vel.x*TFPS, vel.y*TFPS);
-		}
+		spr.play(*currAnim);
+		spr.update(frameTime);
 	}
 	void setAnim(int n){
-		if(n > anims.size() || n < 0) return;
+		if(n > anims.size()) return;
 		currAnim = &anims[n];
 	}
 	virtual void draw(sf::RenderWindow &rw){
 		if(loaded){
-			rw.draw(aspr);
+			rw.draw(spr);
 		}
+	}
+	virtual AnimatedSprite& getAnimatedSprite(){
+		return spr;
 	}
 	virtual bool isLoaded() const{
 		return loaded;
 	}
 	virtual void load(string fn){
-		txts.push_back(*tl.getTexture(fn));
-		loaded = true;
+		if(txt.loadFromFile(fn) == false){
+			filename = "";
+			loaded = false;
+		}else{
+			filename = fn;
+			txt.setSmooth(true);
+			loaded = true;
+			txts.push_back(txt);
+		}
 	}
+	
 	virtual void setPos(float x, float y){
 		if(loaded){
-			aspr.setPosition(x,y);
+			pos.x = x;
+			pos.y = y;
+			spr.setPosition(pos);
 		}
 	}
 	virtual void setVel(float x, float y){
@@ -382,13 +360,13 @@ public:
 		}
 	}
 	virtual float getHeight() const{
-		return aspr.getScale().y;
+		return spr.getScale().y;
 	}
 	virtual float getWidth() const{
-		return aspr.getScale().x;
+		return spr.getScale().x;
 	}
 	virtual sf::Rect<float> getBoundingRect() const{
-		return aspr.getGlobalBounds();
+		return spr.getGlobalBounds();
 	}
 	virtual sf::Vector2f getVel(){
 		if(loaded){
@@ -398,7 +376,7 @@ public:
 	}
 	virtual sf::Vector2f getPos(){
 		if(loaded){
-			return aspr.getPosition();
+			return pos;
 		}
 		return sf::Vector2f();
 	}
@@ -410,79 +388,124 @@ public:
 		}
 		return false;
 	}
-
-	//RAFFEH PLS IMPLEMENT NUDGE TY 
 	virtual void collide(Node* n){
-		sf::Rect<float> mine = getBoundingRect();
-		sf::Rect<float> other = n->getBoundingRect();
-		sf::Rect<float> inter = sf::Rect<float>();
-		if(mine.intersects(other, inter)){
-			cout<< getVel().x << " "<< getVel().y  << endl;
-			if(getPos().x > inter.left){
-				cout << "LEFT" << endl;
-				cout << "BOUNCE " << -(other.left + n->getWidth()) + inter.left - aspr.getOrigin().x/2 << endl;
-				getAnimatedSprite().move(-(other.left + n->getWidth()) + inter.left - aspr.getOrigin().x/2,0);
-				setVel(0,getVel().y);
-			}else{
-				cout << "RIGHT" << endl;
-				setVel(0,getVel().y);
-			} 
-			if(getPos().y > inter.top){
-				cout << "TOP" <<endl;
-				setVel(getVel().x,0);
-			}else{
-				cout << "BOTTOM" << endl;
-				setVel(getVel().x,0);
-			}
-		}
+	}
+
+	void collideTile(Node* n){
+		
+	}
+
+};
+
+class Floor : public Node{
+public:
+	Floor(){
+		load("textures/floor.png");
+		assert(isLoaded());
+		getSprite().setScale(WIDTH/512.0f,FLOOR_HEIGHT/512.0f);
+		setPos(0,HEIGHT - FLOOR_HEIGHT);
+		cstatic = true;
+	}
+	~Floor(){};
+
+	void draw(sf::RenderWindow& rw){
+		Node::draw(rw);
 	}
 };
 
-class Tile : public Node{
+class Wall : public Node{
 public:
-	Tile(int x = 0, int y = 0){
+	enum Wall_Type{
+		Top, Left, Right
+	};
+	Wall(){};
+	Wall(Wall_Type wt){
+		switch(wt){
+		case Top:
+			spr.setScale(WIDTH*1.0f,5.0f);
+			setPos(0,0);
+			break;
+		case Left:
+			spr.setScale(5.0f,HEIGHT*1.0f);
+			setPos(0,0);
+			break;
+		case Right:
+			spr.setScale(5.0f,HEIGHT*1.0f);
+			setPos(WIDTH-5.0f,0);
+			break;
+		default:
+			break;
+		}
+		cstatic = true;
+	};
+	~Wall(){};
+
+
+	void draw(sf::RenderWindow& rw){
+		Node::draw(rw);
+	}
+
+	void collide(Node *n){
+		
+	}
+
+};
+
+class Tile : public Node{
+static sf::Texture txt;
+public:
+	Tile(){
 		load("textures/floor.png");
 		assert(isLoaded());
 		spr.setScale(TILE_SIZE/512.0,TILE_SIZE/512.0);
-		setPos(x,y);
-		//spr.setOrigin(12.5,12.51);
+	}
+	Tile(int x, int y){
+		pos.x = x;
+		pos.y = y;
+		load("textures/floor.png");
+		spr.setScale(TILE_SIZE/512.0,TILE_SIZE/512.0);
+		spr.setPosition(pos);
+		assert(isLoaded());
 	}
 	~Tile(){};
-
-	//RAFFEH PLS IMPLEMENT INELASTIC COLLISION BETWEEN ALL NODES
-	//NUDGE AND APPLY FORCE
-	//use getVel().move(x,y) to apply force depends on what side siya galing
-	//kasi need to implement gravity
+	
 	void collide(Node* n){
-		 
+		sf::Rect<float> mine = getBoundingRect();
+		sf::Rect<float> other = n->getBoundingRect();
+		
+		if( (other.left + other.width) > mine.left && (other.top < mine.top)){
+			if(other.left < mine.left){
+				cout << "resolved 1" << endl;
+				n->setPos(other.left - other.width,n->getPos().y);
+				other = n->getBoundingRect();
+				//return;
+			}
+		} 
 
-		//if( (other.left + other.width) > mine.left && (other.top < mine.top)){
-		//	if(other.left < mine.left){
-		//		cout << "resolved 1" << endl;
-		//		n->setPos(other.left - other.width,n->getPos().y);
-		//		other = n->getBoundingRect();
-		//		//return;
-		//	}
-		//}else if( other.left < mine.left + mine.width && (other.top < mine.top) ){
-		//	if(other.left + other.width > mine.left + mine.width){
-		//		cout << "resolved 2" << endl;
-		//		n->setPos(other.left + other.width,n->getPos().y);
-		//		other = n->getBoundingRect();
-		//		//return;
-		//	}
-		//}else if( other.top < mine.top + mine.height && (other.left + other.width > mine.left || other.left < mine.left + mine.width) ){
-		//	if(other.top + other.height > mine.top + mine.height){
-		//		cout << "resolved 3" <<endl;
-		//		n->setPos(n->getPos().x,mine.top + mine.height);
-		//		other = n->getBoundingRect();
-		//	}
-		//}else if((other.top + other.height > mine.top) && (other.left + other.width > mine.left || other.left < mine.left + mine.width)){
-		//	if(other.top < mine.top){
-		//		cout << "resolved 4" << endl;
-		//		n->setPos(n->getPos().x,mine.top - other.height);
+		if( other.left < mine.left + mine.width && (other.top < mine.top) ){
+			if(other.left + other.width > mine.left + mine.width){
+				cout << "resolved 2" << endl;
+				n->setPos(other.left + other.width,n->getPos().y);
+				other = n->getBoundingRect();
+				//return;
+			}
+		}
+	
+		if( other.top < mine.top + mine.height && (other.left + other.width > mine.left || other.left < mine.left + mine.width) ){
+			if(other.top + other.height > mine.top + mine.height){
+				cout << "resolved 3" <<endl;
+				n->setPos(n->getPos().x,mine.top + mine.height);
+				other = n->getBoundingRect();
+			}
+		}
 
-		//	}
-		//}
+		if((other.top + other.height > mine.top) && (other.left + other.width > mine.left || other.left < mine.left + mine.width)){
+			if(other.top < mine.top){
+				cout << "resolved 4" << endl;
+				n->setPos(n->getPos().x,mine.top - other.height);
+
+			}
+		}
 	}
 
 	void draw(sf::RenderWindow& rw){
@@ -490,7 +513,7 @@ public:
 	}
 };
 
-vector<Node> tiles;
+vector<Tile> tiles;
 
 
 class NodeManager{
@@ -557,15 +580,6 @@ public:
 			objx++;
 		}
 	}
-
-	void moveAll(){
-		map<string,Node*>::const_iterator itr = nodes.begin();
-
-		while(itr != nodes.end()){
-			itr->second->move();
-			itr++;
-		}
-	}
 };
 
 NodeManager nodeManager;
@@ -573,8 +587,8 @@ NodeManager nodeManager;
 void collideTiles(){
 	for(int i = 0; i < tiles.size(); i++){
 		if(nodeManager.get("Player")->isColliding(&tiles[i])){
-			//tiles[i].collide(nodeManager.get("Player"));
-			((Player*)nodeManager.get("Player"))->collide(&tiles[i]);
+			tiles[i].collide(nodeManager.get("Player"));
+			//(Player)nodeManager.get("Player")->collideTile(tiles[i]);
 		}
 	}
 }
@@ -587,28 +601,28 @@ void collision(){
 void input(){
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
 		nodeManager.get("Player")->setVel(-PLAYER_VX,0);
-		nodeManager.get("Player")->getAnimatedSprite().setScale(-1,1);
-		nodeManager.get("Player")->setAnim(1);
+		((Player*)nodeManager.get("Player"))->getAnimatedSprite().setScale(-1,1);
+		((Player*)nodeManager.get("Player"))->setAnim(1);
 	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
 		nodeManager.get("Player")->setVel(PLAYER_VX,0);
-		nodeManager.get("Player")->getAnimatedSprite().setScale(1,1);
-		nodeManager.get("Player")->setAnim(1);
+		((Player*)nodeManager.get("Player"))->getAnimatedSprite().setScale(1,1);
+		((Player*)nodeManager.get("Player"))->setAnim(1);
 	}else{
 		nodeManager.get("Player")->setVel(0,0);
-		nodeManager.get("Player")->setAnim(0);
+		((Player*)nodeManager.get("Player"))->setAnim(0);
 	}
 
 	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
 		nodeManager.get("Player2")->setVel(-PLAYER_VX,0);
-		nodeManager.get("Player2")->getAnimatedSprite().setScale(-1,1);
-		nodeManager.get("Player2")->setAnim(1);
+		((Player*)nodeManager.get("Player2"))->getAnimatedSprite().setScale(-1,1);
+		((Player*)nodeManager.get("Player2"))->setAnim(1);
 	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
 		nodeManager.get("Player2")->setVel(PLAYER_VX,0);
-		nodeManager.get("Player2")->getAnimatedSprite().setScale(1,1);
-		nodeManager.get("Player2")->setAnim(1);
+		((Player*)nodeManager.get("Player2"))->getAnimatedSprite().setScale(1,1);
+		((Player*)nodeManager.get("Player2"))->setAnim(1);
 	}else{
 		nodeManager.get("Player2")->setVel(0,0);
-		nodeManager.get("Player2")->setAnim(0);
+		((Player*)nodeManager.get("Player2"))->setAnim(0);
 	}
 }
 
@@ -619,7 +633,6 @@ void animate(){
 void logic(){
 	nodeManager.updateAll();
 	collision();
-	nodeManager.moveAll();
 }
 
 void render(){
@@ -643,21 +656,19 @@ void gameInit(){
 
 	nodeManager.add("Player",new Player());
 	nodeManager.add("Player2",new Player());
-	nodeManager.get("Player2")->getAnimatedSprite().setScale(-1,1);
-	nodeManager.get("Player")->setPos(P1_START_POSX,HEIGHT-PLAYER_H/2.0);
-	nodeManager.get("Player2")->setPos(P2_START_POSX,HEIGHT-PLAYER_H/2.0);
+	((Player*)nodeManager.get("Player2"))->getAnimatedSprite().setScale(-1,1);
+	nodeManager.get("Player")->setPos(P1_START_POSX,HEIGHT-FLOOR_HEIGHT-PLAYER_H);
+	nodeManager.get("Player2")->setPos(P2_START_POSX,HEIGHT-FLOOR_HEIGHT-PLAYER_H);
 
 	for(int i = 0; i < MAP_ROWS; i++){
 		for(int j = 0; j <  MAP_COLUMNS - 1; j++){
-			//if(i == 0 || i == MAP_ROWS-1 || j == 0 || j == MAP_COLUMNS-2) map_grid[i][j] =1;
-			//else map_grid[i][j] = 0;
 			map_grid[i][j] = 0;
 		}
 	}
-	/*for(int i = 0; i < MAP_COLUMNS; i++){
+	for(int i = 0; i < MAP_COLUMNS; i++){
 		map_grid[MAP_ROWS - 1][i] = 0;
-	}*/
-	map_grid[14][0]= 1;
+	}
+	map_grid[13][1]= 1;
 	for(int i = 0; i < MAP_ROWS; i++){
 		for(int j = 0; j < MAP_COLUMNS - 1; j++){
 			if(map_grid[i][j] == 1){
