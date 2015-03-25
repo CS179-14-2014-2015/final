0afa37bf017518dce1e535707bf44efbd86bae6b
John Joseph Ugalino <john.ugalino@obf.ateneo.edu>
2015-03-25 16:43:24 +0800

    added animation

--- src/main.cpp
+++ src/main.cpp
@@ -37,10 +37,166 @@ const double GRAV = 9.8/(FPS*1.0);
 
 sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "Sorta Jousting");
 sf::Clock gameClock;
-sf::Time startTime;
+sf::Time frameTime;
 sf::RectangleShape shape(sf::Vector2f(FLOOR_WIDTH,FLOOR_HEIGHT));
 
 //some references from http://www.gamefromscratch.com/page/Game-From-Scratch-CPP-Edition.aspx
+//and animation references from https://github.com/LaurentGomila/SFML/wiki/Source:-AnimatedSprite
+
+class Animation{
+private:
+	std::vector<sf::IntRect> m_frames;
+	const sf::Texture* m_texture;
+public:
+	Animation() : m_texture(NULL){}
+	~Animation(){
+		m_texture = NULL;
+	}
+	void addFrame(sf::IntRect rect){
+		m_frames.push_back(rect);
+	}
+	void setSpriteSheet(const sf::Texture& texture){
+		m_texture = &texture;
+	}
+	const sf::Texture* getSpriteSheet() const{
+		return m_texture;
+	}
+	std::size_t getSize() const{
+		return m_frames.size();
+	}
+	const sf::IntRect& getFrame(std::size_t n) const{
+		return m_frames[n];
+	}
+};
+
+class AnimatedSprite : public sf::Drawable, public sf::Transformable{
+private:
+	const Animation* m_animation;
+    sf::Time m_frameTime;
+    sf::Time m_currentTime;
+    std::size_t m_currentFrame;
+    bool m_isPaused;
+    bool m_isLooped;
+    const sf::Texture* m_texture;
+    sf::Vertex m_vertices[4];
+public:
+	AnimatedSprite() : m_animation(NULL), m_frameTime(sf::seconds(0.001)), m_currentFrame(0), m_isPaused(true), m_isLooped(false), m_texture(NULL){}
+	AnimatedSprite(sf::Time frameTime, bool paused, bool looped) : m_animation(NULL), m_frameTime(frameTime), m_currentFrame(0), m_isPaused(paused), m_isLooped(looped), m_texture(NULL){}
+	~AnimatedSprite(){
+		m_animation = NULL;
+		m_texture = NULL;
+	}
+
+	void setAnimation(const Animation& animation){
+		m_animation = &animation;
+		m_texture = m_animation->getSpriteSheet();
+		m_currentFrame = 0;
+		setFrame(m_currentFrame, true);
+	}
+	void setFrameTime(sf::Time time){
+		m_frameTime = time;
+	}
+	void play(){
+		m_isPaused = false;
+	}
+	void play(const Animation& animation){
+		if (getAnimation() != &animation)
+			setAnimation(animation);
+		play();
+	}
+	void pause(){
+		m_isPaused = true;
+	}
+	void stop(){
+		m_isPaused = true;
+		m_currentFrame = 0;
+		setFrame(m_currentFrame, true);
+	}
+	void setLooped(bool looped){
+		m_isLooped = looped;
+	}
+	void setColor(const sf::Color& color){
+		// Update the vertices' color
+		m_vertices[0].color = color;
+		m_vertices[1].color = color;
+		m_vertices[2].color = color;
+		m_vertices[3].color = color;
+	}
+	const Animation* getAnimation() const{
+		return m_animation;
+	}
+	sf::FloatRect getLocalBounds() const{
+		sf::IntRect rect = m_animation->getFrame(m_currentFrame);
+		float width = static_cast<float>(std::abs(rect.width));
+		float height = static_cast<float>(std::abs(rect.height));
+		return sf::FloatRect(0.f, 0.f, width, height);
+	}
+	sf::FloatRect getGlobalBounds() const{
+		return getTransform().transformRect(getLocalBounds());
+	}
+	bool isLooped() const{
+		return m_isLooped;
+	}
+	bool isPlaying() const{
+		return !m_isPaused;
+	}
+	sf::Time getFrameTime() const{
+		return m_frameTime;
+	}
+	void setFrame(std::size_t newFrame, bool resetTime){
+		if (m_animation){
+			//calculate new vertex positions and texture coordiantes
+			sf::IntRect rect = m_animation->getFrame(newFrame);
+
+			m_vertices[0].position = sf::Vector2f(0.f, 0.f);
+			m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
+			m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
+			m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
+
+			float left = static_cast<float>(rect.left) + 0.0001f;
+			float right = left + static_cast<float>(rect.width);
+			float top = static_cast<float>(rect.top);
+			float bottom = top + static_cast<float>(rect.height);
+
+			m_vertices[0].texCoords = sf::Vector2f(left, top);
+			m_vertices[1].texCoords = sf::Vector2f(left, bottom);
+			m_vertices[2].texCoords = sf::Vector2f(right, bottom);
+			m_vertices[3].texCoords = sf::Vector2f(right, top);
+		}
+
+		if (resetTime) m_currentTime = sf::Time::Zero;
+	}
+	void update(sf::Time deltaTime){
+		// if not paused and we have a valid animation
+		if (!m_isPaused && m_animation){
+			// add delta time
+			m_currentTime += deltaTime;
+			// if current time is bigger then the frame time advance one frame
+			if (m_currentTime >= m_frameTime){
+				// reset time, but keep the remainder
+				m_currentTime = sf::microseconds(m_currentTime.asMicroseconds() % m_frameTime.asMicroseconds());
+				// get next Frame index
+				if (m_currentFrame + 1 < m_animation->getSize()) m_currentFrame++;
+				else{
+					// animation has ended
+					m_currentFrame = 0; // reset to start
+					if (!m_isLooped){
+						m_isPaused = true;
+					}
+				}
+				// set the current frame, not reseting the time
+				setFrame(m_currentFrame, false);
+			}
+		}
+	}
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const{
+		if (m_animation && m_texture){
+			states.transform *= getTransform();
+			states.texture = m_texture;
+			target.draw(m_vertices, 4, sf::Quads, states);
+		}
+	}
+};
 
 class Node{
 protected:
@@ -100,12 +256,7 @@ public:
 	virtual sf::Rect<float> getBoundingRect() const{
 		sf::Vector2f size = spr.getScale();
 		
-		return sf::Rect<float>(
-			pos.x - size.x/2,
-			pos.y - size.y/2,
-			pos.x + size.x/2,
-			pos.y + size.y/2
-			);
+		return sf::Rect<float>(pos, size);
 	}
 	virtual sf::Vector2f getVel(){
 		if(loaded){
@@ -136,17 +287,97 @@ public:
 };
 
 class Player : public Node{
+protected:
+	vector<Animation> anims;
+	AnimatedSprite spr;
+	Animation* currAnim;
 public:
-	Player(){
-		load("textures/player.png");
+	Player() : spr(sf::seconds(0.05),true,false) {
+		load("textures/idleplayer.png");
+
+		anims.push_back(*new Animation);
+		anims[0].setSpriteSheet(txt);
+		for(int i = 0; i < 30; i++){
+			anims[0].addFrame(sf::IntRect((i%5)*25,(i/5)*50,25,50));
+		}
+		currAnim = &anims[0];
 		assert(isLoaded());
-		getSprite().setScale(PLAYER_W/256,PLAYER_H/512);
 		getSprite().setOrigin(getSprite().getScale().x/2, getSprite().getScale().y/2);
 	};
 	~Player(){};
 
-	void draw(sf::RenderWindow& rw){
-		Node::draw(rw);
+	void update(){
+		if(loaded){
+			setPos(pos.x + vel.x*TFPS, pos.y + vel.y*TFPS);
+		}
+		spr.play(*currAnim);
+		spr.update(frameTime);
+	}
+	virtual void draw(sf::RenderWindow &rw){
+		if(loaded){
+			rw.draw(spr);
+		}
+	}
+	virtual bool isLoaded() const{
+		return loaded;
+	}
+	virtual void load(string fn){
+		if(txt.loadFromFile(fn) == false){
+			filename = "";
+			loaded = false;
+		}else{
+			filename = fn;
+			txt.setSmooth(true);
+			loaded = true;
+		}
+	}
+	
+	virtual void setPos(float x, float y){
+		if(loaded){
+			pos.x = x;
+			pos.y = y;
+			spr.setPosition(pos);
+		}
+	}
+	virtual void setVel(float x, float y){
+		if(loaded){
+			vel.x = x;
+			vel.y = y;
+		}
+	}
+	virtual float getHeight() const{
+		return spr.getScale().y;
+	}
+	virtual float getWidth() const{
+		return spr.getScale().x;
+	}
+	virtual sf::Rect<float> getBoundingRect() const{
+		sf::Vector2f size = spr.getScale();
+		
+		return sf::Rect<float>(pos, size);
+	}
+	virtual sf::Vector2f getVel(){
+		if(loaded){
+			return vel;
+		}
+		return sf::Vector2f();
+	}
+	virtual sf::Vector2f getPos(){
+		if(loaded){
+			return pos;
+		}
+		return sf::Vector2f();
+	}
+	virtual bool isColliding(Node* n){
+		//collision test here raffeh
+		//collision test here raffeh
+		//collision test here raffeh
+		return false;
+	}
+	virtual void collide(Node* n){
+		//collision reaction here raffeh
+		//collision reaction here raffeh
+		//collision reaction here raffeh
 	}
 };
 
@@ -322,7 +553,7 @@ void render(){
 }
 
 void gameLoop(){
-	startTime = gameClock.getElapsedTime();
+	frameTime = gameClock.restart();
 	input();
 	animate();
 	logic();
